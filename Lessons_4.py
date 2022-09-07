f8473a6a2a53bb85b259b0b6661571ca8322ef63
Vadym Lymar <99604901+VadymLymar@users.noreply.github.com>
2022-09-07 23:36:11 +0300

    Lessons4

---
+++ Lessons_4.py
@@ -0,0 +1,10 @@
+# value = input("Увести ціле число: ")
+# try:
+#     value = int(value)
+#     result = 1 / value
+#     print (value, result)
+# except (ValueError, TypeError):
+#     print("Це не ціле число!!")
+# except ZeroDivisionError:
+#     print ("Not 0")
+#
